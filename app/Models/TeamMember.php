780d779c2d13ee74bf5802e5f51b740b9bacd0bf
sahhoutiamine <midoriaduko@gmail.com>
2026-01-09 11:58:29 +0100

    add TeamMember class

---
+++ app/Models/TeamMember.php
@@ -0,0 +1,76 @@
+<?php
+
+// abstract class TeamMember {
+//     protected $id;
+//     protected $title;
+//     protected $description;
+//     protected $projectId;
+//     protected $assigneeId;
+//     protected $reporterId;
+//     protected $priority;
+//     protected $status;
+//     protected $estimatedHours;
+//     protected $actualHours;
+//     protected $dueDate;
+//     protected $createdAt;
+//     protected $updatedAt;
+    
+// }
+
+abstract class TeamMember {
+    protected $id;
+    protected $username;
+    protected $email;
+    protected $pw;
+    protected $teamId; 
+    protected $createdAt;
+
+
+
+
+    public function __construct($id, $username, $email, $pw, $teamId, $createdAt) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->pw = $pw;
+        $this->teamId = $teamId;
+        $this->createdAt = $createdAt;
+    }
+
+
+    public function getId() {
+        return $this->id;
+    }
+    public function getUsername() {
+        return $this->username;
+    }
+    public function getEmail() {
+        return $this->email;
+    }
+    public function getPassword() {
+        return $this->pw;
+    }
+    public function getTeamId() {
+        return $this->teamId;
+    }
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
+    
+
+
+    abstract public function canCreateProject() : bool;
+    abstract public function canAssignTasks() : bool;
+    abstract public function getRolePermissions() : array;
+
+    public function verifyPassword($pw) {
+        if ($this->pw === $pw) {
+            return true;
+        }
+        return false;
+    };
+    public function setPassword($newPw) {
+        $this->pw->$newPw;
+    }
+}
+
