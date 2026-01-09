07c65b03f58d31e0885bcbd41c0192d821904a11
sahhoutiamine <midoriaduko@gmail.com>
2026-01-09 12:50:35 +0100

    add methods to commentable interface

--- app/Interfaces/Commentable.php
+++ app/Interfaces/Commentable.php
@@ -3,5 +3,8 @@
 namespace App\Interfaces;
 
 interface Commentable {
-
+    public function addComment(string $content, int $userId): int;
+    public function removeComment(int $commentId): bool;
+    public function getComments(): array;
+    public function getCommentCount(): int;
 }
